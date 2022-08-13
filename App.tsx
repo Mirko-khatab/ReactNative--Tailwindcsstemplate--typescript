8022f9f874b4ff03704b29453b691ca14a3de139
Mirko-khatab <mirkokawa7@gmail.com>
2022-08-13 17:14:26 +0300

    add project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+import { TailwindProvider } from 'tailwindcss-react-native'
+
+import React from 'react'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <TailwindProvider>
+      <View className="flex-1 items-center justify-center bg-red-300">
+        <Text className="text-white">Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </TailwindProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
